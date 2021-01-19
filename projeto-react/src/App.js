dfd678717bcb2ee49862bce561e2ce183df81709
Rafael30s <rafael.santos@bandtec.com.br>
2021-01-18 23:37:20 -0300

    Dia 2

--- projeto-react/src/App.js
+++ projeto-react/src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react"
-import ListaDeNotas from "./componentes/ListaDeNotas"
-import { FormualarioCadastro } from "./componentes/FormularioCadastro";
+import ListaDeNotas from "./componentes/ListaDeNotas/"
+import FormularioCadastro from "./componentes/FormularioCadastro/";
+import "./assets/App.css";
+import "./assets/index.css";
 
 class App extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      notas:[]
+    }
+  }
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas: novoArrayNotas
+    }
+    this.setState(novoEstado);
+    }
+
   render(){
     return (
-      <section>
-        <FormualarioCadastro/>
-       <ListaDeNotas/>
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
